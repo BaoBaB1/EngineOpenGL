c971bb0020c8e1c50c241e72098630afe2808881
BaoBaB1 <lexa562@ukr.net>
2025-04-23 20:42:45 +0300

    refactor uvs calculations for cube

--- src/ge/Cube.cpp
+++ src/ge/Cube.cpp
@@ -8,33 +8,33 @@ namespace fury
     mesh.vertices().reserve(24);
     mesh.faces().reserve(12);
     for (int i = 0; i < 3; ++i) {
-      mesh.append_vertex(Vertex(-0.5f, -0.5f, -0.5f));
-      mesh.append_vertex(Vertex(0.5f, -0.5f, -0.5f));
-      mesh.append_vertex(Vertex(0.5f, 0.5f, -0.5f));
-      mesh.append_vertex(Vertex(-0.5f, 0.5f, -0.5f));
       mesh.append_vertex(Vertex(-0.5f, -0.5f, 0.5f));
       mesh.append_vertex(Vertex(0.5f, -0.5f, 0.5f));
       mesh.append_vertex(Vertex(0.5f, 0.5f, 0.5f));
       mesh.append_vertex(Vertex(-0.5f, 0.5f, 0.5f));
+      mesh.append_vertex(Vertex(-0.5f, -0.5f, -0.5f));
+      mesh.append_vertex(Vertex(0.5f, -0.5f, -0.5f));
+      mesh.append_vertex(Vertex(0.5f, 0.5f, -0.5f));
+      mesh.append_vertex(Vertex(-0.5f, 0.5f, -0.5f));
     }
-    // back
-    mesh.append_face(Face({ 1, 0, 3 }));
-    mesh.append_face(Face({ 1, 3, 2 }));
     // front
-    mesh.append_face(Face({ 4, 5, 6 }));
-    mesh.append_face(Face({ 4, 6, 7 }));
+    mesh.append_face(Face({ 0, 1, 2 }));
+    mesh.append_face(Face({ 0, 2, 3 }));
+    // back
+    mesh.append_face(Face({ 5, 4, 7 }));
+    mesh.append_face(Face({ 5, 7, 6 }));
     // bottom
-    mesh.append_face(Face({ 8, 9, 13 }));
-    mesh.append_face(Face({ 8, 13, 12 }));
+    mesh.append_face(Face({ 12, 13, 9 }));
+    mesh.append_face(Face({ 12, 9, 8 }));
     // top
-    mesh.append_face(Face({ 15, 14, 10 }));
-    mesh.append_face(Face({ 15, 10, 11 }));
+    mesh.append_face(Face({ 11, 10, 14 }));
+    mesh.append_face(Face({ 11, 14, 15 }));
     // left
-    mesh.append_face(Face({ 16, 20, 23 }));
-    mesh.append_face(Face({ 16, 23, 19 }));
+    mesh.append_face(Face({ 20, 16, 19 }));
+    mesh.append_face(Face({ 20, 19, 23 }));
     // right
-    mesh.append_face(Face({ 21, 17, 18 }));
-    mesh.append_face(Face({ 21, 18, 22 }));
+    mesh.append_face(Face({ 17, 21, 22 }));
+    mesh.append_face(Face({ 17, 22, 18 }));
 
     // by default we create cube with flat shading
     set_shading_mode(ShadingMode::FLAT_SHADING);
@@ -44,39 +44,20 @@ namespace fury
     int cnt = 0;
     for (const auto& face : mesh.faces())
     {
-      for (int i = 0; i < Face::size; ++i)
+      Vertex& v1 = mesh.vertices()[face.data[0]];
+      Vertex& v2 = mesh.vertices()[face.data[1]];
+      Vertex& v3 = mesh.vertices()[face.data[2]];
+      if (cnt % 2 == 0)
       {
-        Vertex& v = mesh.vertices()[face.data[i]];
-        if (cnt % 2 == 0)
-        {
-          if (i == 0)
-          {
-            v.texture = glm::vec2();
-          }
-          else if (i == 1)
-          {
-            v.texture = glm::vec2(1.f, 0.f);
-          }
-          else
-          {
-            v.texture = glm::vec2(1.f, 1.f);
-          }
-        }
-        else
-        {
-          if (i == 0)
-          {
-            v.texture = glm::vec2();
-          }
-          else if (i == 1)
-          {
-            v.texture = glm::vec2(1.f, 1.f);
-          }
-          else
-          {
-            v.texture = glm::vec2(0.f, 1.f);
-          }
-        }
+        v1.texture = glm::vec2();
+        v2.texture = glm::vec2(1.f, 0.f);
+        v3.texture = glm::vec2(1.f, 1.f);
+      }
+      else
+      {
+        v1.texture = glm::vec2();
+        v2.texture = glm::vec2(1.f, 1.f);
+        v3.texture = glm::vec2(0.f, 1.f);
       }
       cnt++;
     }
