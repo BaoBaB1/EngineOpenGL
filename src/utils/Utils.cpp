f37721aceb19c9332163441ef12577f8ee70f4e2
BaoBaB1 <lexa562@ukr.net>
2025-05-09 20:24:20 +0300

    fix root dir search

--- src/utils/Utils.cpp
+++ src/utils/Utils.cpp
@@ -1,22 +1,23 @@
 #include "Utils.hpp"
 #include "core/Logger.hpp"
 
-static constexpr const char project_root_dir_name[] = "EngineOpenGL";
-
 namespace
 {
   std::filesystem::path find_root_dir()
   {
     auto path = fury::utils::get_exe_path();
+    bool found = false;
     while (path.has_relative_path())
     {
       path = path.parent_path();
-      if (path.filename() == project_root_dir_name)
+      // find top-most assets folder within exe path
+      if (std::filesystem::exists(path / "assets"))
       {
+        found = true;
         break;
       }
     }
-    if (path.empty())
+    if (!found)
     {
       fury::Logger::critical("Could not find project root directory from exe path {}.", fury::utils::get_exe_path().string());
       throw std::runtime_error("Could not find project root directory");
