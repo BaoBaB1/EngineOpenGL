ac6032de9de6b9cd72ac9e7f7a1e1d690f2239d2
BaoBaB1 <lexa562@ukr.net>
2026-05-08 21:00:06 +0300

    Add named event listeners

--- src/core/Event.hpp
+++ src/core/Event.hpp
@@ -3,125 +3,125 @@
 #include <vector>
 #include <functional>
 #include <memory>
+#include <string>
+#include <string_view>
 
 namespace fury
 {
-	template<typename... Args>
-	class EventListener
-	{
-	public:
-		virtual void notify(Args... args) const = 0;
-		virtual void* owner() const { return nullptr; }
-		virtual ~EventListener() = default;
-	};
+  template<typename... Args>
+  class EventListener
+  {
+  public:
+    virtual void notify(Args... args) const = 0;
+    virtual void* get_owner_instance() const { return nullptr; }
+    virtual std::string_view get_name() const { return ""; }
+    virtual ~EventListener() = default;
+  };
 
-	template<typename ReturnType, typename... Args>
-	class FunctionListener : public EventListener<Args...>
-	{
-	public:
-		using Func = std::function<ReturnType(Args...)>;
-		using FuncPtr = ReturnType(*)(Args...);
-		FunctionListener(const Func& func) : m_func(func) {}
-		FunctionListener(FuncPtr func_ptr) : m_func(func_ptr) {}
-		void notify(Args... args) const override
-		{
-			m_func(args...);
-		}
-	private:
-		Func m_func;
-	};
+  template<typename ReturnType, typename... Args>
+  class FunctionListener : public EventListener<Args...>
+  {
+  public:
+    using Func = std::function<ReturnType(Args...)>;
+    using FuncPtr = ReturnType (*)(Args...);
+    FunctionListener(const Func& func) : m_func(func) {}
+    FunctionListener(FuncPtr func_ptr) : m_func(func_ptr) {}
+    void notify(Args... args) const override { m_func(args...); }
+  private:
+    Func m_func;
+  };
 
-	template<typename Class, typename ReturnType, typename... Args>
-	class InstanceListener : public EventListener<Args...>
-	{
-	public:
-		using ClassFunc = ReturnType(Class::*)(Args...);
-		InstanceListener(Class* instance, ClassFunc func) : m_func(func), m_instance(instance) {}
-		void* owner() const override { return m_instance; }
-		void notify(Args... args) const override
-		{
-			(m_instance->*m_func)(args...);
-		}
-	private:
-		Class* m_instance;
-		ClassFunc m_func;
-	};
+  template<typename ReturnType, typename... Args>
+  class NamedFunctionListener : public FunctionListener<ReturnType, Args...>
+  {
+  public:
+    using Parent = FunctionListener<ReturnType, Args...>;
+    using Func = std::function<ReturnType(Args...)>;
+    using FuncPtr = ReturnType (*)(Args...);
+    // Apparently CTAD does not work with nested dependent aliases (at least with GCC),
+    // so specifying ctor argument like Parent::Func or Parent::FuncPtr fails type deduction...
+    NamedFunctionListener(const std::string& name, const Func& func) : Parent(func) { m_name = name; }
+    NamedFunctionListener(const std::string& name, FuncPtr func_ptr) : Parent(func_ptr) { m_name = name; }
+    std::string_view get_name() const override { return m_name; }
+  private:
+    std::string m_name;
+  };
 
-	template<typename... Args>
-	class Event
-	{
-	public:
-		using Listener = EventListener<Args...>;
+  template<typename Class, typename ReturnType, typename... Args>
+  class InstanceListener : public EventListener<Args...>
+  {
+  public:
+    using ClassFunc = ReturnType (Class::*)(Args...);
+    InstanceListener(Class* instance, ClassFunc func) : m_func(func), m_instance(instance) {}
+    void* get_owner_instance() const override { return m_instance; }
+    void notify(Args... args) const override { (m_instance->*m_func)(args...); }
+  private:
+    Class* m_instance;
+    ClassFunc m_func;
+  };
 
-		void notify(Args... args) const
-		{
-			for (const auto& h : m_listeners)
-			{
-				h->notify(args...);
-			}
-		}
+  template<typename Class, typename ReturnType, typename... Args>
+  class NamedInstanceListener : public InstanceListener<Class, ReturnType, Args...>
+  {
+  public:
+    using Parent = InstanceListener<Class, ReturnType, Args...>;
+    using ClassFunc = ReturnType (Class::*)(Args...);
+    NamedInstanceListener(const std::string& name, Class* instance, ClassFunc func) : Parent(instance, func)
+    {
+      m_name = name;
+    }
+    std::string_view get_name() const override { return m_name; }
+  private:
+    std::string m_name;
+  };
 
-		Event& operator +=(Listener* listener)
-		{
-			m_listeners.emplace_back(listener);
-			return *this;
-		}
+  template<typename... Args>
+  class Event
+  {
+  public:
+    using Listener = EventListener<Args...>;
 
-		bool operator -=(Listener* listener)
-		{
-			return this->operator-=(static_cast<void*>(listener));
-		}
+    void notify(Args... args) const
+    {
+      for (const auto& h : m_listeners)
+      {
+        h->notify(args...);
+      }
+    }
 
-		bool operator -=(void* listener)
-		{
-			for (size_t i = 0; i < m_listeners.size(); i++)
-			{
-				if (m_listeners[i].get() == listener)
-				{
-					m_listeners.erase(m_listeners.begin() + i);
-					return true;
-				}
-			}
-			return false;
-		}
+    Event& add_listener(Listener* listener)
+    {
+      m_listeners.emplace_back(listener);
+      return *this;
+    }
 
-		bool remove_by_owner(void* owner)
-		{
-			for (size_t i = 0; i < m_listeners.size(); i++)
-			{
-				if (m_listeners[i].get()->owner() == owner)
-				{
-					m_listeners.erase(m_listeners.begin() + i);
-					return true;
-				}
-			}
-			return false;
-		}
+    Event& remove_listener(Listener* listener)
+    {
+      std::erase_if(m_listeners, [=](const auto& lstnr) { return lstnr.get() == listener; });
+      return *this;
+    }
 
-		Listener* get(void* owner)
-		{
-			for (const auto& listener : m_listeners)
-			{
-				if (listener.get()->owner() == owner)
-				{
-					return listener.get();
-				}
-			}
-			return nullptr;
-		}
+    Event& operator-=(Listener* listener) { return remove_listener(listener); }
 
-		Listener* get(size_t idx) { return m_listeners[idx].get(); }
+    Event& operator+=(Listener* listener) { return add_listener(listener); }
 
-		Listener* back() { return m_listeners.back().get(); }
+    bool remove_listener_by_name(const std::string& name)
+    {
+      return std::erase_if(m_listeners, [&](const auto& lstnr) { return lstnr->get_name() == name; }) > 0;
+    }
 
-		void unregister_all()
-		{
-			m_listeners.clear();
-		}
+    bool remove_listener_by_instance(void* owner)
+    {
+      return std::erase_if(m_listeners, [=](const auto& lstnr) { return lstnr->get_owner_instance() == owner; }) > 0;
+    }
 
-		size_t listeners_count() const { return m_listeners.size(); }
+    Listener* get_listener(size_t idx) { return m_listeners[idx].get(); }
 
-	private:
-		std::vector<std::unique_ptr<Listener>> m_listeners;
-	};
-}
+    void remove_all_listeners() { m_listeners.clear(); }
+
+    size_t listeners_count() const { return m_listeners.size(); }
+
+  private:
+    std::vector<std::unique_ptr<Listener>> m_listeners;
+  };
+} // namespace fury
